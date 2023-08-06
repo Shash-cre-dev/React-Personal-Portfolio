4ef6b5cd0defe8d1b97088aa7e00fe1a9b8add10
Shashank <shashwritescode@gmail.com>
2023-08-06 20:34:33 +0530

    Portfolio Component

--- react-portfolio/src/components/services/Services.jsx
+++ react-portfolio/src/components/services/Services.jsx
@@ -45,7 +45,7 @@ const Services = () => {
         className="services__container container mySwiper">
                 {services.map(({name, title, description}, index) =>{
                     return (
-                        <SwiperSlide className="services__item card card-one">
+                        <SwiperSlide className="services__item card card-one" key={index}>
                             <span className="services__subtitle text-cs">{name}</span>
 
                             <h3 className="services__title">{title}</h3>
